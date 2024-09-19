24f00001aa6f9850e786e591c95418c89c76caa8
Ayush <142293028+AntimatterHere@users.noreply.github.com>
2024-09-19 17:21:28 +0530

    Update decisiontreeproject.py

--- decisiontreeproject.py
+++ decisiontreeproject.py
@@ -1,39 +1,62 @@
 import pandas as pd
 import numpy as np
-data = pd.read_csv('creditcard.csv')
+import matplotlib.pyplot as plt
+data = pd.read_csv("creditcard.csv")
+# data=data.drop(["Unnamed: 5","Unnamed: 6"])
 print(data.head())
-# checking for the null values in dataset
+
 print(data.isnull().sum())
-# let’s have a look at the type of transaction
+
+# Exploring transaction type
 print(data.type.value_counts())
+
 type = data["type"].value_counts()
 transactions = type.index
 quantity = type.values
+
+import plotly.express as px
+figure = px.pie(data, 
+            values=quantity, 
+            names=transactions,hole = 0.5, 
+            title="Distribution of Transaction Type")
+figure.show()
+
+newbalanceOrig=list(data["newbalanceOrig"])
+errors=["C865699625"]
+for i in range(len(newbalanceOrig)):
+  if newbalanceOrig[i]==errors[0]:
+    newbalanceOrig[i]=0
+data["newbalanceOrig"]=newbalanceOrig
+
+data["type"] = data["type"].map({"CASH_OUT": 1, "PAYMENT": 2, 
+                                 "CASH_IN": 3, "TRANSFER": 4,
+                                 "DEBIT": 5})
+#data["isFraud"] = data["isFraud"].map({0: "No Fraud", 1: "Fraud"})
+print(data.head())
+
 # Checking correlation
 correlation = data.corr()
 print(correlation["isFraud"].sort_values(ascending=False))
-#transforming the categorical features into numerical
-data["type"] = data["type"].map({"CASH_OUT": 1, "PAYMENT": 2,"CASH_IN": 3, "TRANSFER": 4,"DEBIT": 5})
-data["isFraud"] = data["isFraud"].map({0: "No Fraud", 1: "Fraud"})
-print(data.head())
-#let’s train a classification model
-from sklearn.preprocessing import LabelEncoder
-le=LabelEncoder()
-data["type"]=le.fit_transform(data["type"])
-data["nameOrig"]=le.fit_transform(data["nameOrig"])
-data["nameDest"]=le.fit_tarnsform(data["nameDest"])
+
 # splitting the data
 from sklearn.model_selection import train_test_split
-x = np.array(data[["type", "amount", "oldbalanceOrg", "newbalanceOrig"]])
-y = np.array(data[["isFraud"]])
-#Now let’s train the online payments fraud detection model:
-# training a machine learning model
-from sklearn.tree import DecisionTreeClassifier
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.10, random_state=42)
-model = DecisionTreeClassifier()
+#x = np.array(data[["type", "amount", "oldbalanceOrg", "newbalanceOrig"]])
+#y = np.array(data[["isFraud"]])
+x=data.iloc[:,:4].values
+y=data.iloc[:,-1].values
+print(x)
+#print(y)
+
+from math import nan
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=nan, strategy='mean')
+y=y.reshape(-1,1)
+imputer.fit(y)
+y = imputer.transform(y).reshape(-1)
+print(y)
+
+import xgboost as xgb
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.3, random_state=42)
+model =  xgb.XGBClassifier()
 model.fit(xtrain, ytrain)
 print(model.score(xtest, ytest))
-# prediction
-#features = [type, amount, oldbalanceOrg, newbalanceOrig]
-features = np.array([[4, 9000.60, 9000.60, 0.0]])
-print(model.predict(features))
